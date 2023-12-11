771100ee73a1d5eb179a09bc57314931d5e57626
Franck Grenier <franck.grenier@jurihub.fr>
2023-12-11 10:04:55 +0100

    try hard

--- 3/2.php
+++ 3/2.php
@@ -1,29 +1,43 @@
 <?php
 
 $lines = explode(PHP_EOL, file_get_contents('./input.txt'));
+// coordinates : [x = line, y = offset]
+$targets = [
+    [-1, -1],       [-1, 0],         [-1, 1],
+    [ 0, -1],                        [ 0, 1],
+    [ 1, -1],       [ 1, 0],         [ 1, 1],
+];
 $values = [];
 
 for ($i=0; $i < count($lines); $i++) {
     preg_match_all('/\*/', $lines[$i], $possibleGears, PREG_OFFSET_CAPTURE);
-    // coordinates : [x = line, y = offset]
-    $targets = [
-        [-1, -1],    [-1, 0],     [-1, 1],
-        [ 0, -1],                 [ 0, 1],
-        [ 1, -1],    [ 1, 0],     [ 1, 1]
-    ];
+
     foreach ($possibleGears[0] as $possibleGear) {
-        array_walk($targets, function ($target) use ($lines, $i, $possibleGear) {
+        $gearNumbers = [];
+        foreach($targets as $target) {
             $x = $i + $target[0];
             $y = $possibleGear[1] + $target[1];
 
             if (is_numeric($lines[$x][$y])) {
-                print(substr($lines[$x], 0, $possibleGear[1]) . PHP_EOL);
-                $number = trim(substr(substr($lines[$x], 0, $possibleGear[1]), 3 * $target[1]), '.');
-                print($number . PHP_EOL);
+                print("Found adjacent numeric in line {$lines[$x]} at offest {$y}" . PHP_EOL);
+                $searchOffset = $y - 3;
+                $searchOffset = $searchOffset < 0 ? 0 : $searchOffset;
+                print("Regexp search number at offest {$searchOffset}" . PHP_EOL);
+                if (preg_match('/\d+/', $lines[$x], offset: $searchOffset, matches: $numbers)) {
+                    // TODO: replace found numbers by "..." to avoid overlapsing
+                    preg_replace('/\d+/', '.', substr($lines[$x],$searchOffset, 3));
+                    var_dump($numbers);
+                    print("Found number {$numbers[0]}" . PHP_EOL);
+                    $gearNumbers[] = $numbers[0];
+                };
+                if (count($gearNumbers) === 2) {
+                    print("Found 2 gear numbers {$gearNumbers[0]} * {$gearNumbers[1]}" . PHP_EOL);
+                    array_push($values, $gearNumbers[0] * $gearNumbers[1]);
+                    break 1;
+                }
             }
-        });
+        };
     };
 }
 
-var_dump($values);
 print(array_sum($values));
